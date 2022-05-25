bb035420b4aa2172e617826d6f083dfa3227faf8
Ronaldo Avalos <36484401+Ronaldo-Avalos@users.noreply.github.com>
2022-05-25 20:35:19 +0200

    Add files via upload

---
+++ imgs/main.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Respuestas</title>
+    </head>
+    <body>
+        <h2>Encuesta enviada</h2>
+        <p>Pregunta 1: <?php echo $_POST["pregunta1"]; ?></p>
+        <p>Pregunta 2: <?php echo $_POST["pregunta2"]; ?></p>
+        <p>Pregunta 3: <?php echo $_POST["pregunta3"]; ?></p>
+        <p>Pregunta 4: <?php echo $_POST["pregunta4"]; ?></p>
+        <p>Pregunta 5: <?php echo $_POST["pregunta5"]; ?></p>
+        <p>Pregunta 6: <?php echo $_POST["pregunta6"]; ?></p>
+        <p>Pregunta 7: <?php echo $_POST["pregunta7"]; ?></p>
+
+    </body>
+</html>
